2742be96c10d875f14ad373bbfacb0bd546dbc02
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-08-14 13:15:50 +0900

    feat/#21/영업시간 페이지

--- src/pages/store/hours/HoursPage.jsx
+++ src/pages/store/hours/HoursPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Header from '@/components/common/Header'
-import Title from '@/components/common/Title'
 import backIcon from '@/assets/button-back.svg'
 import LargeOrangeButton from '@/components/common/LargeOrangeButton'
 import BottomNav from '@/components/common/BottomNav'
@@ -9,12 +8,19 @@ import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom'
 const HoursPage = () => {
   const navigator = useNavigate()
+
+  const ButtonWrapper = styled.div`
+    display: flex;
+    margin: 71px auto 0 auto;
+  `
   return (
     <div>
       <Header img={backIcon} title='영업 시간' showImg={true} />
-      <Title text='온동네 가게의 영업시간이에요' />
+
       <HoursTable />
-      <LargeOrangeButton label='영업시간 수정' onBtnClick={() => navigator('/hours/edit')} />
+      <ButtonWrapper>
+        <LargeOrangeButton label='영업시간 수정' onBtnClick={() => navigator('/hours/edit')} />
+      </ButtonWrapper>
       <BottomNav />
     </div>
   )
