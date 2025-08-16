a9f16782f413efa77fe53adf5afab97990a950f4
ss0yaa <syp0205@sookmyung.ac.kr>
2025-08-17 01:10:08 +0900

    feat/#29/선택 카테고리에 따라 마커 찍고 ScrollArea에 보여주기

--- src/pages/customer/map/MainMapPage.jsx
+++ src/pages/customer/map/MainMapPage.jsx
@@ -1,8 +1,7 @@
-// 더미데이터 연동 후 삭제
+// 더미데이터 관련 부분 연동 후 삭제 (dummy, 더미, 삭제로 검색하기)
 import dummyData from './dummyData'
 import React, { useEffect, useState } from 'react'
 import * as S from '@/styles/map/MainMapPage.styles'
-import { useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import axios from 'axios'
 import Header from '@/components/common/Header'
@@ -17,13 +16,13 @@ import KakaoMap from '@/components/common/KakaoMap'
 import iconMarker from '@/assets/icon-default-marker.svg'
 const DEFAULT_CENTER = { lat: 37.5326, lng: 126.9905 } // 기본 좌표
 const DEFAULT_LEVEL = 6 // 기본 확대 정도
-// 시장 좌표 (임시)
+// 시장 좌표
 const MARKET_COORDS = {
-  1: { lat: 37.5343, lng: 126.9615 }, // 용산용문시장
-  2: { lat: 37.5349, lng: 126.994 }, // 이태원시장
-  3: { lat: 37.5503, lng: 126.975 }, // 후암재래시장
-  4: { lat: 37.5532, lng: 126.969 }, // 만리시장
-  5: { lat: 37.5412, lng: 126.9865 }, // 해방촌 신흥시장
+  1: { lat: 37.536618383, lng: 126.959796815 }, // 용산용문시장
+  2: { lat: 37.5338328, lng: 126.990036559 }, // 이태원시장
+  3: { lat: 37.550302514133315, lng: 126.97609391600915 }, // 후암재래시장
+  4: { lat: 37.551207465, lng: 126.963509378 }, // 만리시장
+  5: { lat: 37.54531342552204, lng: 126.98498907716948 }, // 해방촌 신흥시장
 }
 
 const MainMapPage = () => {
@@ -34,12 +33,14 @@ const MainMapPage = () => {
   const [categories, setCategories] = useState([]) // 대분류 리스트
   const [subCategories, setSubCategories] = useState([]) // 소분류 리스트
   const [randomStores, setRandomStores] = useState([]) // 랜덤 가게 리스트
+  const [selectedStores, setSelectedStores] = useState([]) // 사용자 선택 가게 리스트
   const [selectedMainId, setSelectedMainId] = useState(null) // 선택된 대분류 ID (단일 선택)
   const [selectedSubIds, setSelectedSubIds] = useState([]) // 선택된 소분류 ID (복수 선택)
   const [center, setCenter] = useState(DEFAULT_CENTER) // 지도 중심 상태
   const [level, setLevel] = useState(DEFAULT_LEVEL) // 지도 확대 정도
   const [markers, setMarkers] = useState([]) // 마커 배열
-  /*
+
+  /* 맨 처음 지도 가져오기
     useEffect(() => {
     // 'en-US' -> 'en' 형태로 변환
     const lang = (i18n.language || 'en').split('-')[0]
@@ -64,6 +65,55 @@ const MainMapPage = () => {
   }, [i18n.language]) 
   */
 
+  /* 사용자가 특정 시장, 대분류, 소분류 선택 시*/
+  useEffect(() => {
+    const hasMarket = !!market?.value
+    const hasMain = !!selectedMainId
+    const hasSub = Array.isArray(selectedSubIds) && selectedSubIds.length > 0
+    if (!hasMarket && !hasMain && !hasSub) {
+      // 아무것도 선택 안되면 호출 x
+      setSelectedStores([])
+      return
+    }
+
+    // 언어 헤더 가져오기
+    const lang = (i18n.language || 'en').split('-')[0]
+
+    // 쿼리 파라미터 (하나라도 있으면 params에 추가)
+    const params = {}
+    if (hasMarket) params.market = market.value
+    if (hasMain) params.main = selectedMainId
+    if (hasSub) params.sub = selectedSubIds
+    axios
+      .get('http://127.0.0.1:8000/map/filter', {
+        params,
+        paramsSerializer: { indexes: null }, // sub=7&sub=8 형태
+        headers: { 'Accept-Language': lang },
+      })
+      .then((res) => {
+        const result = res.data.data
+        // result 배열 형태로 만들어주기
+        const list = result ? (Array.isArray(result) ? result : [result]) : []
+        setSelectedStores(list)
+
+        // 결과 리스트 마커로 모두 찍기
+        // 마커 객체에 가게 데이터(s) 그대로 같이 들고 다닐 수 있게 설정 (data:s)
+        const storeMarkers = list.map((s) => ({
+          id: `store-${s.id}`,
+          position: { lat: s.lat, lng: s.lng },
+          image: { src: iconMarker, size: { width: 28, height: 28 } },
+          data: s,
+        }))
+
+        setMarkers(storeMarkers)
+      })
+      .catch((err) => {
+        console.log(err)
+        setSelectedStores([])
+        setMarkers([])
+      })
+  }, [market?.value, selectedMainId, selectedSubIds, i18n.language])
+
   // 연동 후 삭제
   useEffect(() => {
     setMarkets(dummyData.data.marketOptions)
@@ -83,9 +133,10 @@ const MainMapPage = () => {
       setLevel(3)
       setMarkers([
         {
-          id: `${opt.label}`,
+          id: `market-${opt.value}`,
           position: mapped,
-          image: { src: iconMarker, size: { width: 32, height: 32 } },
+          image: { src: iconMarker, size: { width: 31, height: 31 } },
+          data: { type: 'market', label: opt.label },
         },
       ])
     } else {
@@ -119,10 +170,16 @@ const MainMapPage = () => {
       <S.MapContainer>
         {/* 카카오 지도 */}
         <KakaoMap center={center} level={level} markers={markers} />
-        {/* 스크롤 영역 */}
+        {/* 스크롤 영역 -> 아무것도 선택 안되면 랜덤 가게, 선택되면 선택된 가게 정보*/}
         <ScrollArea
           title={t('dropdown.stores')}
-          randomStores={dummyData.data.randomStores} // 선택 없을 때 노출
+          randomStores={
+            market?.value
+              ? selectedStores.length
+                ? selectedStores
+                : dummyData.data.randomStores
+              : dummyData.data.randomStores
+          } // 선택 없을 때 노출
           categories={categories}
           selectedMainId={selectedMainId}
           selectedSubIds={selectedSubIds}
