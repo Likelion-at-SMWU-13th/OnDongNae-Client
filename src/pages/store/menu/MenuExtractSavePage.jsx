a8b486a32a26b386588bc8c413682a1d1943d1b3
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-08-19 18:12:26 +0900

    Feat/#47/메뉴 추출 후 저장 페이지 연동

--- src/pages/store/menu/MenuExtractSavePage.jsx
+++ src/pages/store/menu/MenuExtractSavePage.jsx
@@ -8,11 +8,25 @@ import BottomNav from '@/components/common/BottomNav'
 import LargeOrangeButton from '@/components/common/LargeOrangeButton'
 import MenuSave from '@/components/menuManagement/MenuSave'
 import axios from 'axios'
-
+export const Main = styled.main`
+  height: calc(100dvh - 155px);
+  display: flex;
+  flex-direction: column;
+  min-height: 0;
+`
+export const Scroll = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  min-height: 0;
+  -webkit-overflow-scrolling: touch;
+  padding-bottom: calc(env(safe-area-inset-bottom, 0) + 80px);
+`
 const ButtonWapper = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 92px;
+  padding-bottom: 30px;
 `
 const MenuExtractSave = () => {
   const navigate = useNavigate()
@@ -21,7 +35,11 @@ const MenuExtractSave = () => {
 
   const handleSave = () => {
     axios
-      .post(`${apiUrl}/me/menus/save`, { headers: { Authorization: `Bearer ${token}` } })
+      .post(
+        `${apiUrl}/me/menus/allergens/extract`,
+        {},
+        { headers: { Authorization: `Bearer ${token}` } },
+      )
       .then((res) => {
         const AllergensData = res.data
         console.log(res.data) // 연동 확인 후 삭제
@@ -34,16 +52,20 @@ const MenuExtractSave = () => {
       })
   }
   return (
-    <div>
+    <div className='scrollable'>
       <Header img={backIcon} title={'메뉴 관리'} showImg={true} />
-      <DoubleTitle
-        title='메뉴판이 저장되었어요'
-        subtitle='메뉴판을 확인하고 하단의 버튼을 눌러주세요'
-      />
-      <MenuSave />
-      <ButtonWapper>
-        <LargeOrangeButton label='알레르기 정보 추출' onBtnClick={handleSave} />
-      </ButtonWapper>
+      <Main>
+        <Scroll>
+          <DoubleTitle
+            title='메뉴판이 저장되었어요'
+            subtitle='메뉴판을 확인하고 하단의 버튼을 눌러주세요'
+          />
+          <MenuSave />
+          <ButtonWapper>
+            <LargeOrangeButton label='알레르기 정보 추출' onBtnClick={handleSave} />
+          </ButtonWapper>
+        </Scroll>
+      </Main>
       <BottomNav />
     </div>
   )
