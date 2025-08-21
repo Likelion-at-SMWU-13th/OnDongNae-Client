e52ca1ad2b232b8d37cb93f5fb2c3be6461699f0
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-08-21 13:51:12 +0900

    Fix/#53/연동 관련 오류 수정

--- src/pages/customer/course/CourseLoading.jsx
+++ src/pages/customer/course/CourseLoading.jsx
@@ -10,20 +10,23 @@ import CustomerBottomNav from '@/components/common/CustomerBottomNav'
 import axios from 'axios'
 
 const CourseLoading = () => {
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
   const { state } = useLocation()
-  const apiUrl = import.meta.env.VITE_API_URL
+  const API_URL = import.meta.env.VITE_API_URL
   //   const [courses, setCourses] = useState([])
   const navigate = useNavigate()
   console.log(state) // 연동 후 삭제
   // { marketId: 5, withOptionId: 2, atmosphereOptionId: 9 }
 
   useEffect(() => {
+    const lang = (i18n.language || 'en').split('-')[0]
+
     axios
-      .post(`${apiUrl}/courses/recommend`, state, {
+      .post(`${API_URL}/courses/recommend`, state, {
         headers: { 'Accept-Language': lang },
       })
       .then((res) => {
+        console.log(res)
         navigate('/user/course/result', { state: res.data.data })
       })
       .catch((err) => {
