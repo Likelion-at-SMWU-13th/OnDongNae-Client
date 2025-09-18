01c92b659c0a4aedea9d6ea795e24af87c22fa84
ss0yaa <syp0205@sookmyung.ac.kr>
2025-09-18 13:46:48 +0900

    [Setting] 로컬에 https 적용

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import { defineConfig, loadEnv } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: '@', replacement: '/src' }], // '@'를 '/src' 폴더로 대체
-  },
-})
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  const isDevelop = process.env.VITE_DEVELOP === 'true'
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      https: isDevelop
+        ? {
+            key: fs.readFileSync('./localhost-key.pem'),
+            cert: fs.readFileSync('./localhost.pem'),
+          }
+        : false,
+      host: 'localhost',
+      port: 5173,
+    },
+    resolve: {
+      alias: [{ find: '@', replacement: '/src' }],
+    },
+  })
+}
