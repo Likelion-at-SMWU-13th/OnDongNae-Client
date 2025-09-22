141a134483d566bb07b174e8e233bab897d3e3cd
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-09-22 16:40:28 +0900

    [Design] 레이아웃 조정

--- src/pages/customer/overview/OverviewPage.jsx
+++ src/pages/customer/overview/OverviewPage.jsx
@@ -68,7 +68,7 @@ const markers = [
     image: {
       src: mapMaker,
       size: { width: 28, height: 28 },
-      options: { offset: { x: 20, y: 40 } }, // (선택) 클릭 좌표 기준점
+      options: { offset: { x: 14, y: 40 } },
     },
   },
   {
@@ -78,7 +78,7 @@ const markers = [
     image: {
       src: mapMaker,
       size: { width: 28, height: 28 },
-      options: { offset: { x: 20, y: 40 } }, // (선택) 클릭 좌표 기준점
+      options: { offset: { x: 14, y: 40 } },
     },
   },
   {
@@ -88,7 +88,7 @@ const markers = [
     image: {
       src: mapMaker,
       size: { width: 28, height: 28 },
-      options: { offset: { x: 20, y: 40 } }, // (선택) 클릭 좌표 기준점
+      options: { offset: { x: 14, y: 40 } },
     },
   },
   {
@@ -98,7 +98,7 @@ const markers = [
     image: {
       src: mapMaker,
       size: { width: 28, height: 28 },
-      options: { offset: { x: 20, y: 40 } }, // (선택) 클릭 좌표 기준점
+      options: { offset: { x: 14, y: 40 } },
     },
   },
   {
@@ -108,7 +108,7 @@ const markers = [
     image: {
       src: mapMaker,
       size: { width: 28, height: 28 },
-      options: { offset: { x: 20, y: 40 } }, // (선택) 클릭 좌표 기준점
+      options: { offset: { x: 14, y: 40 } },
     },
   },
 ]
